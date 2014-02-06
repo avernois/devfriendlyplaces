a0f6852a2a4721e7cd56198e66496cec2ef10f6c
Sam Cranford <me@nostradamnit.com>
2014-02-06 15:47:09 +0100

    refactored map.js to be a self-executing function to better encapsulate it. Now it exposes a single object, mapMaker with 2 public functions

--- js/map.js
+++ js/map.js
@@ -1,77 +1,83 @@
-function getCityFromUrl() {
-	var hostname = window.location.hostname;
-	var split = hostname.split(".");
-	var city = split[0];
-	if ((split.length < 3) || (city == "www")) {
-		return "toulouse";
+var mapMaker = (function(){
+	function getCityFromUrl() {
+		var hostname = window.location.hostname;
+		var split = hostname.split(".");
+		var city = split[0];
+		if ((split.length < 3) || (city == "www")) {
+			return "toulouse";
+		}
+
+		return city;
 	}
 
-	return city;
-}
+	function getPlaces(city) {
+		var request = new XMLHttpRequest();
+		request.open("GET", "/places/" + city + ".json", false);
+		request.send(null)
 
-function getPlaces(city) {
-	var request = new XMLHttpRequest();
-	request.open("GET", "/places/" + city + ".json", false);
-	request.send(null)
-
-	return JSON.parse(request.responseText) 	
-}
-
-function buildMapFor(city) {
-	var defaultZoom = 14;
-	var places = getPlaces(city);
-
-	var map = L.map('map').setView([places.city.lat, places.city.lon], places.city.defaultZoom);
-
-	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
-	}).addTo(map);
-
-	places.places.forEach(function(place) {
-		L.marker([place.lat, place.lon])
-		.bindPopup(placeToHtml(place))
-		.addTo(map);
-	});
-}
-
-function placeToHtml(place) {
-	return "<b>" + place.name + "</b><br>"
-	+ optionalFieldToHtml("address", place.address)
-	+ optionalFieldToHtml("open hours", place.openHours)
-	+ optionalFieldToHtml("type", place.type)
-	+ optionalUrlToLink("website", place.url)
-	+ optionToHtml("power", place.power)
-	+ optionToHtml("wifi", place.wifi);
-};
-
-function optionalFieldToHtml(label, value) {
-	return value ? label + ": " + value + "<br>" : "";
-}
-
-function optionalUrlToLink(label, value){
-	return value ? label + ": " + "<a href='" + value + "'>" + value + "</a>" + "<br>" : "";
-}
-
-function optionToHtml(label, value) {
-	return label + ": " + optionText(value) + "<br>";
-}
-
-function optionText(value) {
-	if (value !== undefined) {
-		return boolToStr(value.available) + optionalComment(value.comment);
-	} else {
-		return "undefined";
-	}	
-}
-
-function boolToStr(value) {
-	if (value !== undefined) {
-		return value ? "yes" : "no";
-	} else {
-		return 'undefined';
+		return JSON.parse(request.responseText) 	
 	}
-}
 
-function optionalComment(value) {
-	return value ? "(" + value + ")" : "";
-}
+	function buildMapFor(city) {
+		var defaultZoom = 14;
+		var places = getPlaces(city);
+
+		var map = L.map('map').setView([places.city.lat, places.city.lon], places.city.defaultZoom);
+
+		L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+		}).addTo(map);
+
+		places.places.forEach(function(place) {
+			L.marker([place.lat, place.lon])
+			.bindPopup(placeToHtml(place))
+			.addTo(map);
+		});
+	}
+
+	function placeToHtml(place) {
+		return "<b>" + place.name + "</b><br>"
+		+ optionalFieldToHtml("address", place.address)
+		+ optionalFieldToHtml("open hours", place.openHours)
+		+ optionalFieldToHtml("type", place.type)
+		+ optionalUrlToLink("website", place.url)
+		+ optionToHtml("power", place.power)
+		+ optionToHtml("wifi", place.wifi);
+	};
+
+	function optionalFieldToHtml(label, value) {
+		return value ? label + ": " + value + "<br>" : "";
+	}
+
+	function optionalUrlToLink(label, value){
+		return value ? label + ": " + "<a href='" + value + "'>" + value + "</a>" + "<br>" : "";
+	}
+
+	function optionToHtml(label, value) {
+		return label + ": " + optionText(value) + "<br>";
+	}
+
+	function optionText(value) {
+		if (value !== undefined) {
+			return boolToStr(value.available) + optionalComment(value.comment);
+		} else {
+			return "undefined";
+		}	
+	}
+
+	function boolToStr(value) {
+		if (value !== undefined) {
+			return value ? "yes" : "no";
+		} else {
+			return 'undefined';
+		}
+	}
+
+	function optionalComment(value) {
+		return value ? "(" + value + ")" : "";
+	}
+	
+	return {buildMapFor:buildMapFor, 
+			getCityFromUrl:getCityFromUrl};
+	
+})();

8a197170107d8c16f9ad76430d43687bcb9b333e
Antoine <antoine@crafting-labs.fr>
2016-05-19 20:28:32 +0200

    https everywhere

--- js/map.js
+++ js/map.js
@@ -44,7 +44,7 @@ function buildMapFor(location) {
     var locations = getLocations();
     var map = L.map('map').setView([locations[location].lat, locations[location].lon], locations[location].defaultZoom);
     map.on('moveend', onMoveEnd(map, locations));
-    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
     }).addTo(map);
 

98c193df8d4157b0934e71c8e29a272377cbb24f
Antoine <antoine@crafting-labs.fr>
2017-04-29 17:39:40 +0200

    Setup update/view/model

--- Devfriendly.elm
+++ Devfriendly.elm
@@ -2,13 +2,50 @@ module Devfriendly exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
-main =
-    ul [ id "towns" ]
-        [ li [] [ text "Montpellier" ]
-        , li [] [ text "Toulouse" ]
-        , li [] [ text "Paris" ]
-        , li [] [ text "Warsaw" ]
+type Msg
+    = TownSelected
+
+
+type alias Model =
+    { towns : List String }
+
+
+towns : Model
+towns =
+    { towns =
+        [ "Toulouse"
+        , "Montpellier"
+        , "Warswaw"
+        , "Barcelona"
         ]
+    }
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        TownSelected ->
+            ( Debug.log "" model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    ul [ id "towns" ]
+        [ li [ onClick TownSelected ] [ text "Montpellier" ]
+        , li [ onClick TownSelected ] [ text "Toulouse" ]
+        , li [ onClick TownSelected ] [ text "Paris" ]
+        , li [ onClick TownSelected ] [ text "Warsaw" ]
+        ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = ( towns, Cmd.none )
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }

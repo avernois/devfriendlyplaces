87a2f07ef039da7a3ffbbc9025c1abda1871c75d
Travis CI <stephane.langlois@scopyleft.fr>
2017-04-29 20:27:58 +0200

    Add zoom to town

--- Devfriendly.elm
+++ Devfriendly.elm
@@ -26,17 +26,18 @@ type alias Town =
     { name : String
     , latitude : Float
     , longitude : Float
+    , defaultZoom : Int
     }
 
 
 towns : Model
 towns =
     { towns =
-        [ Town "Toulouse" 43.607378 1.4399286
-        , Town "Montpellier" 43.6100219 3.8741615
-        , Town "Warswaw" 52.229676 21.012229
-        , Town "Barcelona" 41.3911671 2.1362266
-        , Town "Lisle sur Tarn" 43.852799 1.810783
+        [ Town "Toulouse" 43.607378 1.4399286 13
+        , Town "Montpellier" 43.6100219 3.8741615 13
+        , Town "Warswaw" 52.229676 21.012229 13
+        , Town "Barcelona" 41.3911671 2.1362266 13
+        , Town "Lisle sur Tarn" 43.852799 1.810783 13
         ]
     }
 
@@ -81,7 +82,7 @@ view model =
 main : Program Never Model Msg
 main =
     Html.program
-        { init = ( towns, consoleJs (Town "" 0 0) )
+        { init = ( towns, consoleJs (Town "" 0 0 0) )
         , view = view
         , update = update
         , subscriptions = \_ -> Sub.none

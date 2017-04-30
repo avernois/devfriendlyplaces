d1b241c558a583cdd539369dfddee2aa70963488
Antoine <antoine@crafting-labs.fr>
2017-04-30 18:21:36 +0200

    Decode places from a Json string

--- Devfriendly.elm
+++ Devfriendly.elm
@@ -106,6 +106,28 @@ townDecoder =
         (field "defaultZoom" Decode.int)
 
 
+placeDecoder : Decoder Place
+placeDecoder =
+    Decode.map3 Place
+        (field "name" Decode.string)
+        (field "lat" Decode.float)
+        (field "lon" Decode.float)
+
+
+placesDecode : String -> List Place
+placesDecode jsonPlaces =
+    let
+        result =
+            Decode.decodeString (Decode.list placeDecoder) jsonPlaces
+    in
+        case result of
+            Ok places ->
+                places
+
+            Err error ->
+                []
+
+
 
 -- MAIN
 
@@ -119,7 +141,7 @@ main : Program Never Model Msg
 main =
     let
         initialModel =
-            { towns = [], places = places }
+            { towns = [], places = placesDecode jsonPlaces }
     in
         Html.program
             { init = ( initialModel, loadTowns townsUrl )
@@ -129,8 +151,54 @@ main =
             }
 
 
-places : List Place
-places =
-    [ Place "L'anartiste" 43.598183 1.4418379
-    , Place "Partisan Café" 44.8305 -0.56764
-    ]
+jsonPlaces : String
+jsonPlaces =
+    """
+[
+    {
+        "name": "Café Contretemps",
+        "lat": 43.600479,
+        "lon": 1.442523
+    },
+    {
+        "name": "Le Café Cerise",
+        "lat": 43.600071,
+        "lon": 1.440335
+    },
+    {
+        "name": "La fabrique",
+        "lat": 43.607378,
+        "lon": 1.4399286
+    },
+    {
+        "name": "Brasserie Père Léon",
+        "lat": 43.600474,
+        "lon": 1.443858999999975
+    },
+    {
+        "name": "The George and Dragon",
+        "lat": 43.607414,
+        "lon": 1.4396000000000413
+    },
+    {
+        "name": "L'impro",
+        "lat": 43.602984,
+        "lon": 1.4416109999999662
+    },
+    {
+        "name": "Les coudes sur la table",
+        "lat": 43.607103,
+        "lon": 1.439847
+    },
+    {
+        "name": "Le Cyrano",
+        "lat": 43.6039657,
+        "lon": 1.4472385
+    },
+    {
+        "name": "Ombres blanches",
+        "lat": 43.603361,
+        "lon": 1.442201
+    }
+]
+"""
